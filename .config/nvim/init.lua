039c26be76661678a30093c87575ec48c0ed21ed
ethan dibble <edibble@pdx.edu>
2025-01-16 21:31:40 -0800

    dotfiles

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+require("config.lazy")
+
+vim.opt.number = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.ruler = true
+vim.opt.wrap = true
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
